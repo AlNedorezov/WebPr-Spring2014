46279bb800cfd82940e2a454dbefbd0fa1784d6b
AlNedorezov <Al73rus@gmail.com>
2014-02-17 10:37:15 +0400

    Сохранение изменений за первую половину занятия 17.02.2014

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<?php
-echo "Text";
-?>
+<!DOCTYPE html>
+<title>HTML Template</title>
+<header>
+    <meta name="keywords" content="bla-bla-bla">
+    <meta name="description" content="bla-bla-bla site">
+</header>
+</body>
+    <?php
+        echo "<p>Text</p>";
+    ?>
+    <h1>Заголовок первого уровня</h1>
+    <h2>Заголовок второго уровня</h2>
+    <h3>Заголовок третьего уровня</h3>
+    <h4>Заголовок четвёртого уровня</h4>
+    <h5>Заголовок пятого уровня</h5>
+    <h6>Заголовок шестого уровня</h6>
+<p>абзац</p>
+<br>
+<hr>
+<i>курсив</i>
+<b>жирный</b>
+<u>подчёркивание</u>
+<s>зачёркивание</s>
+<sub>подстрочный текст</sub>
+<sup>надстрочный текст</sup>
+<br>
+<b><i><s>Пример форматирования <br>текста</s></i></b>
+<br>
+E=mc<sup>2</sup>
+<br>
+H<sub>2</sub>0
+<br>
+<h2>Списки</h2>
+<br>
+<ol>
+    <li>first element</li>
+    <li>second element
+    <ul>
+    <li>first element</li>
+    <li>second element</li>
+    <li>third element</li>
+    </ul>
+    </li>
+    <li>third element</li>
+</ol>
+</body>
+</html>
